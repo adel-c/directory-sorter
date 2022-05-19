0575aac69d966e80ac05da1f7535c7d448174b45
Adel <576853-astran@users.noreply.gitlab.com>
2022-05-19 23:18:04 +0200

    wip

--- down_sorter.py
+++ down_sorter.py
@@ -3,36 +3,39 @@ from mimetypes import MimeTypes
 
 type_map = {
     "video": ["mp4", "mkv", "avi"],
-    "docs": ["odg", "doc", "epub", "txt", "docx", "pdf", "csv"],
+    "docs": ["odg", "doc", "txt", "docx", "pdf", "csv"],
+    "ebooks": ["epub"],
     "archive": ["zip", "7z", "tar", "gz", "tgz", "tar.gz"],
     "iso": ["iso"],
     "images": ["img", "jpeg", "svg"]
 }
 
 
-def mmm(p):
-    print(p)
-    print(os.path.isfile(p))
-    return os.path.isfile(p)
-
-
 def list_all_files_in(directory):
-    directory_with_tail_slash = os.path.join(directory, '')
-    all_children = os.listdir(directory_with_tail_slash)
-    absolute_path = list(map(lambda filename: directory_with_tail_slash + filename, all_children))
+    all_children = os.listdir(directory)
+    absolute_path = list(map(lambda filename: directory + filename, all_children))
     f = filter(os.path.isfile, absolute_path)
     return list(f)
 
 
 def list_all():
     mime = MimeTypes()
+    directory = os.path.join("/home/adel/Téléchargements", '')
+    listdir = list_all_files_in(directory)
+    create_target_dirs(directory)
 
-    listdir = list_all_files_in("/home/adel/Téléchargements")
     print(listdir)
 
     for f in listdir:
         print(f)
         print("\t\t" + str(mime.guess_type(f)))
+
+
+def create_target_dirs(directory):
+    for dir in type_map.keys():
+        target_dir = os.path.join(directory, dir)
+        if not os.path.exists(target_dir):
+            os.mkdir(target_dir)
 
 
 if __name__ == '__main__':
